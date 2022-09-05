67b4e3c474feb7b91e63cef333706284c533e343
SL programming:git config --global user.name SL <you@example.com>
2022-09-05 11:05:47 +0630

    first working

---
+++ script.js
@@ -0,0 +1,40 @@
+const clock = document.querySelector(".clock");
+const secondTag = document.querySelector(".second");
+const minuteTag = document.querySelector(".minute");
+const hourTag = document.querySelector(".hour");
+let degForSecond = 0;
+let degForMinute = 0;
+let degForHour = 0;
+let second = 0;
+let minute = 0;
+let hour = 0;
+
+// add number Pointer
+for (i = 0; i < 12; i++) {
+  const number = document.createElement("div");
+  number.classList.add("number");
+  if (i == 0 || i == 6 || i == 3 || i == 9) {
+    number.style.clipPath = `polygon(94% 0%, 100% 0%, 100% 100%, 94% 100%)`;
+  }
+  number.style.transform = `rotate(${(360 / 12) * i}deg)`;
+  clock.append(number);
+}
+
+const intervalId = setInterval(() => {
+  degForSecond += 6;
+  secondTag.style.transform = `rotate(${degForSecond}deg)`;
+  second += 1;
+  console.log(second);
+  if (second == 60) {
+    degForMinute += 6;
+    second = 0;
+    minute += 1;
+    minuteTag.style.transform = `rotate(${degForMinute}deg)`;
+  }
+  if (minute == 60) {
+    degForHour += 6;
+    minute = 0;
+    hour += 1;
+    hourTag.style.transform = `rotate(${degForHour}deg)`;
+  }
+}, 1000);
