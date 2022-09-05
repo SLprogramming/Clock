893ef9fd758bdabb529918423d42405899032d14
SL programming:git config --global user.name SL <you@example.com>
2022-09-05 15:15:37 +0630

    added digital clock

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const clock = document.querySelector(".clock");
 const secondTag = document.querySelector(".second");
 const minuteTag = document.querySelector(".minute");
 const hourTag = document.querySelector(".hour");
+const digital = document.querySelector(".digital");
 let degForSecond = 0;
 let degForMinute = 0;
 let degForHour = 0;
 let second = 0;
 let minute = 0;
-let hour = 0;
-
+let hour = 12;
 // add number Pointer
 for (i = 0; i < 12; i++) {
   const number = document.createElement("div");
@@ -24,7 +24,6 @@ const intervalId = setInterval(() => {
   degForSecond += 6;
   secondTag.style.transform = `rotate(${degForSecond}deg)`;
   second += 1;
-  console.log(second);
   if (second == 60) {
     degForMinute += 6;
     second = 0;
@@ -35,6 +34,12 @@ const intervalId = setInterval(() => {
     degForHour += 6;
     minute = 0;
     hour += 1;
+    if (hour > 12) {
+      hour = 1;
+    }
     hourTag.style.transform = `rotate(${degForHour}deg)`;
   }
+  digital.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${
+    minute < 10 ? `0${minute}` : minute
+  }`;
 }, 1000);
